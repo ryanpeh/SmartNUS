28a653dcbb51bf04fef6f145c961ab7f0c650263
Cedric Ong <cedricong99@gmail.com>
2021-09-25 20:29:01 +0800

    Add Unit tests for AddMcqCommandParser

---
+++ src/test/java/seedu/address/logic/commands/AddMcqCommandTest.java
@@ -0,0 +1,9 @@
+package seedu.address.logic.commands;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddMcqCommandTest {
+    //TODO: Add testing when execute uses Question object
+}
