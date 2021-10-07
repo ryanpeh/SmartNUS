44882d9f57454dfe6cdb53edfbdf9a9f0292d667
Neo Jing Xuan <neojxuan@gmail.com>
2021-10-07 15:51:23 +0800

    Change prefixes so tokenizer can be used

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -6,12 +6,12 @@ package seedu.address.logic.parser;
 public class CliSyntax {
 
     /* Prefix definitions */
-    public static final Prefix PREFIX_NAME = new Prefix("/n ");
-    public static final Prefix PREFIX_IMPORTANCE = new Prefix("/i ");
-    public static final Prefix PREFIX_TAG = new Prefix("/t ");
+    public static final Prefix PREFIX_NAME = new Prefix("n/");
+    public static final Prefix PREFIX_IMPORTANCE = new Prefix("i/");
+    public static final Prefix PREFIX_TAG = new Prefix("t/");
 
-    public static final Prefix PREFIX_QUESTION = new Prefix("/qns ");
-    public static final Prefix PREFIX_OPTION = new Prefix("/opt ");
-    public static final Prefix PREFIX_ANSWER = new Prefix("/ans ");
+    public static final Prefix PREFIX_QUESTION = new Prefix("qn/");
+    public static final Prefix PREFIX_OPTION = new Prefix("opt/");
+    public static final Prefix PREFIX_ANSWER = new Prefix("ans/");
 
 }
