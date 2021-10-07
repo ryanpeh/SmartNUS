89f593e3137d7e5ff74a8c75c0f51b00cb74e599
Neo Jing Xuan <neojxuan@gmail.com>
2021-10-07 13:36:55 +0800

    Update prefix format

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -6,9 +6,9 @@ package seedu.address.logic.parser;
 public class CliSyntax {
 
     /* Prefix definitions */
-    public static final Prefix PREFIX_NAME = new Prefix("n/");
-    public static final Prefix PREFIX_IMPORTANCE = new Prefix("i/");
-    public static final Prefix PREFIX_TAG = new Prefix("t/");
+    public static final Prefix PREFIX_NAME = new Prefix("/n");
+    public static final Prefix PREFIX_IMPORTANCE = new Prefix("/i");
+    public static final Prefix PREFIX_TAG = new Prefix("/t");
 
     public static final Prefix PREFIX_QUESTION = new Prefix("/qns");
     public static final Prefix PREFIX_OPTION = new Prefix("/opt");
