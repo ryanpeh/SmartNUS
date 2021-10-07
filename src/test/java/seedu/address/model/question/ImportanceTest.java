efa4669961e4e6cb44b33984382fe8a0f827a175
Neo Jing Xuan <neojxuan@gmail.com>
2021-10-07 13:19:47 +0800

    fix style

--- src/test/java/seedu/address/model/question/ImportanceTest.java
+++ src/test/java/seedu/address/model/question/ImportanceTest.java
@@ -3,8 +3,8 @@ package seedu.address.model.question;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static seedu.address.testutil.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
