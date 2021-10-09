611901b89fe024dad93d90b32da57c6e7200a267
aryagiat <arya.giat@gmail.com>
2021-10-09 20:17:23 +0800

    Add quiz related commands

---
+++ src/main/java/seedu/address/logic/commands/quiz/PrevQuestionCommand.java
@@ -0,0 +1,30 @@
+package seedu.address.logic.commands.quiz;
+
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.CommandResult;
+import seedu.address.model.Model;
+
+import static java.util.Objects.requireNonNull;
+
+public class PrevQuestionCommand extends Command {
+    public static final String COMMAND_WORD = "prev";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Quizes the previous question, takes no arguments";
+
+    public static final String MESSAGE_SUCCESS = "Got Previous Quiz!";
+    public PrevQuestionCommand() {}
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        // TODO: use the QuizManager.prevQuestion() method
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        // TODO: In future, check if the attributes (if any) for the QuizCommand are the same
+        return other == this // short circuit if same object
+                || other instanceof PrevQuestionCommand; // instanceof handles nulls
+    }
+}
