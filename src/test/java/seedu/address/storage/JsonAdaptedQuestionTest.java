9a0f259fc5478c7a659515f369e60d811e0cfbcb
Agentum07 <sheamus9712@gmail.com>
2021-10-12 00:18:27 +0800

    Add test for storing invalid choice

--- src/test/java/seedu/address/storage/JsonAdaptedQuestionTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedQuestionTest.java
@@ -12,6 +12,7 @@ import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.choice.Choice;
 import seedu.address.model.question.Importance;
 import seedu.address.model.question.Name;
 
@@ -19,12 +20,16 @@ public class JsonAdaptedQuestionTest {
     private static final String INVALID_IMPORTANCE = "+651234";
     private static final String INVALID_NAME = " ";
     private static final String INVALID_TAG = "#friend";
+    private static final String INVALID_CHOICE = " ";
 
     private static final String VALID_NAME = BENSON.getName().toString();
     private static final String VALID_IMPORTANCE = BENSON.getImportance().toString();
     private static final List<JsonAdaptedTag> VALID_TAGS = BENSON.getTags().stream()
             .map(JsonAdaptedTag::new)
             .collect(Collectors.toList());
+    private static final List<JsonAdaptedChoice> VALID_CHOICES = BENSON.getChoices().stream()
+            .map(JsonAdaptedChoice::new).
+            collect(Collectors.toList());
 
     @Test
     public void toModelType_validQuestionDetails_returnsQuestion() throws Exception {
@@ -35,14 +40,14 @@ public class JsonAdaptedQuestionTest {
     @Test
     public void toModelType_invalidName_throwsIllegalValueException() {
         JsonAdaptedQuestion question =
-                new JsonAdaptedQuestion(INVALID_NAME, VALID_IMPORTANCE, VALID_TAGS);
+                new JsonAdaptedQuestion(INVALID_NAME, VALID_IMPORTANCE, VALID_TAGS, VALID_CHOICES);
         String expectedMessage = Name.MESSAGE_CONSTRAINTS;
         assertThrows(IllegalValueException.class, expectedMessage, question::toModelType);
     }
 
     @Test
     public void toModelType_nullName_throwsIllegalValueException() {
-        JsonAdaptedQuestion question = new JsonAdaptedQuestion(null, VALID_IMPORTANCE, VALID_TAGS);
+        JsonAdaptedQuestion question = new JsonAdaptedQuestion(null, VALID_IMPORTANCE, VALID_TAGS, VALID_CHOICES);
         String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, Name.class.getSimpleName());
         assertThrows(IllegalValueException.class, expectedMessage, question::toModelType);
     }
@@ -50,14 +55,14 @@ public class JsonAdaptedQuestionTest {
     @Test
     public void toModelType_invalidImportance_throwsIllegalValueException() {
         JsonAdaptedQuestion question =
-                new JsonAdaptedQuestion(VALID_NAME, INVALID_IMPORTANCE, VALID_TAGS);
+                new JsonAdaptedQuestion(VALID_NAME, INVALID_IMPORTANCE, VALID_TAGS, VALID_CHOICES);
         String expectedMessage = Importance.MESSAGE_CONSTRAINTS;
         assertThrows(IllegalValueException.class, expectedMessage, question::toModelType);
     }
 
     @Test
     public void toModelType_nullImportance_throwsIllegalValueException() {
-        JsonAdaptedQuestion question = new JsonAdaptedQuestion(VALID_NAME, null, VALID_TAGS);
+        JsonAdaptedQuestion question = new JsonAdaptedQuestion(VALID_NAME, null, VALID_TAGS, VALID_CHOICES);
         String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, Importance.class.getSimpleName());
         assertThrows(IllegalValueException.class, expectedMessage, question::toModelType);
     }
@@ -67,7 +72,14 @@ public class JsonAdaptedQuestionTest {
         List<JsonAdaptedTag> invalidTags = new ArrayList<>(VALID_TAGS);
         invalidTags.add(new JsonAdaptedTag(INVALID_TAG));
         JsonAdaptedQuestion question =
-                new JsonAdaptedQuestion(VALID_NAME, VALID_IMPORTANCE, invalidTags);
+                new JsonAdaptedQuestion(VALID_NAME, VALID_IMPORTANCE, invalidTags, VALID_CHOICES);
         assertThrows(IllegalValueException.class, question::toModelType);
+    }
+
+    @Test
+    public void toModelType_invalidChoices_throwsIllegalValueException() {
+        List<JsonAdaptedChoice> invalidChoices = new ArrayList<>();
+        assertThrows(IllegalArgumentException.class,
+                () -> invalidChoices.add(new JsonAdaptedChoice(new Choice(INVALID_CHOICE, true))));
     }
 }
