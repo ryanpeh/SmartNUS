cfab59d2b5187b2602e6c02c9ddf9dec9a5ceef7
Ryan Peh <qwertz0808@gmail.com>
2021-10-14 22:28:56 +0800

    Fix checkstyle issues

--- src/test/java/seedu/address/logic/parser/QuizInputParserTest.java
+++ src/test/java/seedu/address/logic/parser/QuizInputParserTest.java
@@ -3,7 +3,6 @@ package seedu.address.logic.parser;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import java.nio.file.Path;
@@ -19,7 +18,6 @@ import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.quiz.NextQuestionCommand;
 import seedu.address.logic.commands.quiz.PrevQuestionCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.quiz.QuizManager;
 import seedu.address.storage.JsonSmartNusStorage;
