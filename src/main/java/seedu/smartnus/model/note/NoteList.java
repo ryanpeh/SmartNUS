9942f1590a832ed349d3909942456b48ab67bd27
Agentum07 <sheamus9712@gmail.com>
2021-10-19 17:49:05 +0800

    create a note list

--- src/main/java/seedu/smartnus/model/note/NoteList.java
+++ src/main/java/seedu/smartnus/model/note/NoteList.java
@@ -1,2 +1,70 @@
-package seedu.smartnus.model.note;public class NoteList {
+package seedu.smartnus.model.note;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.smartnus.commons.util.CollectionUtil.requireAllNonNull;
+
+import java.util.Iterator;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class NoteList implements Iterable<Note> {
+    private final ObservableList<Note> internalList = FXCollections.observableArrayList();
+    private final ObservableList<Note> internalUnmodifiableList =
+            FXCollections.unmodifiableObservableList(internalList);
+
+    /**
+     * Adds a note to the list.
+     */
+    public void add(Note toAdd) {
+        requireNonNull(toAdd);
+        // todo: check if note is duplicated
+        internalList.add(toAdd);
+    }
+
+    /**
+     * Replaces the note {@code target} in the list with {@code editedNote}.
+     * {@code target} must exist in the list.
+     * The note identity of {@code editedNote} must not be the same as another existing question in the list.
+     */
+    public void setNote(Note target, Note editedNote) {
+        requireAllNonNull(target, editedNote);
+
+        int index = internalList.indexOf(target);
+        if (index == -1) {
+            // todo: create an exception class for notes
+            throw new RuntimeException();
+        }
+
+        internalList.set(index, editedNote);
+    }
+
+    public void setNote(NoteList replacement) {
+        requireNonNull(replacement);
+        internalList.setAll(replacement.internalList);
+    }
+
+    /**
+     * Returns the backing list as an unmodifiable {@code ObservableList}.
+     */
+    public ObservableList<Note> asUnmodifiableObservableList() {
+        return internalUnmodifiableList;
+    }
+
+    @Override
+    public Iterator<Note> iterator() {
+        return internalList.iterator();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof NoteList // instanceof handles nulls
+                && internalList.equals(((NoteList) other).internalList));
+    }
+
+    @Override
+    public int hashCode() {
+        return internalList.hashCode();
+    }
 }
