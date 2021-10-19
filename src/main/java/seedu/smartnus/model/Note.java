e0c44ab86a404083c87edb33cc670a0e5ce429e9
Agentum07 <sheamus9712@gmail.com>
2021-10-19 17:14:30 +0800

    Create note class

--- src/main/java/seedu/smartnus/model/Note.java
+++ src/main/java/seedu/smartnus/model/Note.java
@@ -1,2 +1,65 @@
-package seedu.smartnus.model;public class Note {
+package seedu.smartnus.model;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.smartnus.commons.util.AppUtil.checkArgument;
+
+import seedu.smartnus.model.choice.Choice;
+
+public class Note {
+
+    public static final String MESSAGE_CONSTRAINTS = "Notes can take any values, and they cannot not be blank";
+
+    /*
+     * The first character of the choice must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
+     */
+    public static final String VALIDATION_REGEX = "[^\\s].*";
+
+    private final String title;
+
+    /**
+     * Constructs a {@code Note}.
+     *
+     * @param title Title of the note.
+     */
+    public Note(String title) {
+        requireNonNull(title);
+        checkArgument(isValidNoteTitle(title), MESSAGE_CONSTRAINTS);
+        this.title = title;
+    }
+
+    /**
+     * Returns true if a given string is a valid choice title.
+     */
+    public static boolean isValidNoteTitle(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return title;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof Note)) {
+            return false;
+        }
+
+        Note otherNote = (Note) other;
+        return otherNote.getTitle().equals(getTitle());
+    }
+
+    @Override
+    public int hashCode() {
+        return title.hashCode();
+    }
 }
