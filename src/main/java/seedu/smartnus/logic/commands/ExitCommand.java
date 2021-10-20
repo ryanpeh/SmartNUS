9271b6dd71f5a8bd9dfcfedffdf8be37754eb464
aryagiat <arya.giat@gmail.com>
2021-10-20 14:47:17 +0800

    Fix: after exiting a quiz, show all the questions

--- src/main/java/seedu/smartnus/logic/commands/ExitCommand.java
+++ src/main/java/seedu/smartnus/logic/commands/ExitCommand.java
@@ -2,6 +2,8 @@ package seedu.smartnus.logic.commands;
 
 import seedu.smartnus.model.Model;
 
+import static seedu.smartnus.model.Model.PREDICATE_SHOW_ALL_QUESTIONS;
+
 /**
  * Terminates the program.
  */
@@ -13,6 +15,7 @@ public class ExitCommand extends Command {
 
     @Override
     public CommandResult execute(Model model) {
+        model.updateFilteredQuestionList(PREDICATE_SHOW_ALL_QUESTIONS);
         return new CommandResult(MESSAGE_EXIT_ACKNOWLEDGEMENT, false, true, false);
     }
 
