d903cd939f2ed369b36143b511aaf0bb3e6e7105
Agentum07 <sheamus9712@gmail.com>
2021-10-20 22:48:33 +0800

    fix checkStyle in test

--- src/test/java/seedu/smartnus/logic/parser/AddNoteCommandParserTest.java
+++ src/test/java/seedu/smartnus/logic/parser/AddNoteCommandParserTest.java
@@ -1,10 +1,10 @@
 package seedu.smartnus.logic.parser;
 
 import static seedu.smartnus.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.smartnus.logic.commands.CommandTestUtil.INVALID_NOTE_DESC;
 import static seedu.smartnus.logic.commands.CommandTestUtil.PREAMBLE_WHITESPACE;
 import static seedu.smartnus.logic.commands.CommandTestUtil.VALID_NOTE_1;
 import static seedu.smartnus.logic.commands.CommandTestUtil.VALID_NOTE_2;
-import static seedu.smartnus.logic.commands.CommandTestUtil.INVALID_NOTE_DESC;
 import static seedu.smartnus.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.smartnus.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
@@ -39,28 +39,30 @@ public class AddNoteCommandParserTest {
         assertParseFailure(parser, INVALID_NOTE_DESC , expectedMessage);
     }
 
-//    @Test
-//    void parse_fieldsInvalid_failure() {
-//        // TODO: Add tests for duplicate options and answer
-//        // invalid answer
-//        assertParseFailure(parser, QUESTION_DESC_1 + OPTIONS_DESC_1 + INVALID_ANSWER_DESC + IMPORTANCE_DESC_1,
-//                Choice.MESSAGE_CONSTRAINTS);
-//        // 1 invalid option with 2 valid
-//        assertParseFailure(parser, QUESTION_DESC_1 + OPTION_DESC_1 + INVALID_OPTION_DESC + OPTION_DESC_3
-//                + ANSWER_DESC_1 + IMPORTANCE_DESC_1, Choice.MESSAGE_CONSTRAINTS);
-//        // not enough options
-//        assertParseFailure(parser,
-//                QUESTION_DESC_1 + OPTION_DESC_1 + OPTION_DESC_3 + ANSWER_DESC_1 + IMPORTANCE_DESC_1,
-//                MultipleChoiceQuestion.MESSAGE_INCORRECT_NUMBER_OF_CHOICES);
-//        // too many options
-//        assertParseFailure(parser, QUESTION_DESC_1 + ANSWER_DESC_1 + IMPORTANCE_DESC_1 + OPTIONS_DESC_2,
-//                MultipleChoiceQuestion.MESSAGE_INCORRECT_NUMBER_OF_CHOICES);
-//        // invalid question
-//        assertParseFailure(parser, INVALID_QUESTION_DESC + OPTIONS_DESC_1 + ANSWER_DESC_1 + IMPORTANCE_DESC_1,
-//                Name.MESSAGE_CONSTRAINTS);
-//        // invalid importance
-//        assertParseFailure(parser,
-//                QUESTION_DESC_1 + OPTIONS_DESC_1 + ANSWER_DESC_1 + INVALID_IMPORTANCE_DESC,
-//                Importance.MESSAGE_CONSTRAINTS);
-//    }
+    /*
+    @Test
+    void parse_fieldsInvalid_failure() {
+        // TODO: Add tests for duplicate options and answer
+        // invalid answer
+        assertParseFailure(parser, QUESTION_DESC_1 + OPTIONS_DESC_1 + INVALID_ANSWER_DESC + IMPORTANCE_DESC_1,
+                Choice.MESSAGE_CONSTRAINTS);
+        // 1 invalid option with 2 valid
+        assertParseFailure(parser, QUESTION_DESC_1 + OPTION_DESC_1 + INVALID_OPTION_DESC + OPTION_DESC_3
+                + ANSWER_DESC_1 + IMPORTANCE_DESC_1, Choice.MESSAGE_CONSTRAINTS);
+        // not enough options
+        assertParseFailure(parser,
+                QUESTION_DESC_1 + OPTION_DESC_1 + OPTION_DESC_3 + ANSWER_DESC_1 + IMPORTANCE_DESC_1,
+                MultipleChoiceQuestion.MESSAGE_INCORRECT_NUMBER_OF_CHOICES);
+        // too many options
+        assertParseFailure(parser, QUESTION_DESC_1 + ANSWER_DESC_1 + IMPORTANCE_DESC_1 + OPTIONS_DESC_2,
+                MultipleChoiceQuestion.MESSAGE_INCORRECT_NUMBER_OF_CHOICES);
+        // invalid question
+        assertParseFailure(parser, INVALID_QUESTION_DESC + OPTIONS_DESC_1 + ANSWER_DESC_1 + IMPORTANCE_DESC_1,
+                Name.MESSAGE_CONSTRAINTS);
+        // invalid importance
+        assertParseFailure(parser,
+                QUESTION_DESC_1 + OPTIONS_DESC_1 + ANSWER_DESC_1 + INVALID_IMPORTANCE_DESC,
+                Importance.MESSAGE_CONSTRAINTS);
+    }
+    */
 }
