d471bad8e7417278153575c896a22e19252a4ed6
aryagiat <arya.giat@gmail.com>
2021-10-20 19:50:00 +0800

    increase coverage

--- src/test/java/seedu/smartnus/logic/parser/QuizCommandParserTest.java
+++ src/test/java/seedu/smartnus/logic/parser/QuizCommandParserTest.java
@@ -8,9 +8,11 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.smartnus.commons.core.index.Index;
 import seedu.smartnus.logic.commands.quiz.QuizCommand;
 import seedu.smartnus.logic.parser.quiz.QuizCommandParser;
 import seedu.smartnus.model.question.predicate.ShowAllQuestionsPredicate;
+import seedu.smartnus.model.question.predicate.ShowQuestionIndexPredicate;
 import seedu.smartnus.model.question.predicate.TagsContainKeywordsPredicate;
 
 
@@ -32,5 +34,10 @@ class QuizCommandParserTest {
         assertParseSuccess(parser, "", new QuizCommand(new ShowAllQuestionsPredicate()));
         assertParseSuccess(parser, " t/ST2334 t/CS2103T",
                 new QuizCommand(new TagsContainKeywordsPredicate(List.of("ST2334", "CS2103T"))));
+    }
+
+    @Test
+    void parse_withIndex() {
+        assertParseSuccess(parser, " 1", new QuizCommand(new ShowQuestionIndexPredicate(Index.fromOneBased(1))));
     }
 }
