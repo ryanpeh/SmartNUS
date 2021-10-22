194753e40fdcc49ffb7692a99921697933e82b1f
Ryan Peh <qwertz0808@gmail.com>
2021-10-22 10:53:55 +0800

    Change invalid quiz command feedback

--- src/main/java/seedu/smartnus/commons/core/Messages.java
+++ src/main/java/seedu/smartnus/commons/core/Messages.java
@@ -10,6 +10,6 @@ public class Messages {
     public static final String MESSAGE_INVALID_QUESTION_DISPLAYED_INDEX = "The question index provided is invalid";
     public static final String MESSAGE_QUESTIONS_LISTED_OVERVIEW = "%1$d questions listed!";
     public static final String MESSAGE_INVALID_MCQ_ANSWER_FORMAT = "Invalid answer format!"
-            + " Acceptable answers are 'a', 'b', 'c', 'd'\n";
+            + " Acceptable answers are 'a', 'b', 'c', 'd' (Case insensitive)\n";
 
 }
