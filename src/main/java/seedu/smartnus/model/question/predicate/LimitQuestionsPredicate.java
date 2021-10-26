4c8265e5055c3b72d92fce5ecc067fc474104e53
Cedric Ong <cedricong99@gmail.com>
2021-10-26 17:16:24 +0800

    Create predicate to limit quiz questions

---
+++ src/main/java/seedu/smartnus/model/question/predicate/LimitQuestionsPredicate.java
@@ -0,0 +1,33 @@
+package seedu.smartnus.model.question.predicate;
+
+import java.util.function.Predicate;
+
+import seedu.smartnus.model.question.Question;
+
+
+
+public class LimitQuestionsPredicate implements Predicate<Question> {
+
+    public int questionLimit;
+
+    public LimitQuestionsPredicate(int limit) {
+        this.questionLimit = limit;
+    }
+
+    @Override
+    public boolean test(Question question) {
+        if (questionLimit > 0) {
+            questionLimit--;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof LimitQuestionsPredicate // instanceof handles nulls
+                && this.questionLimit == ((LimitQuestionsPredicate) other).questionLimit); // state check
+    }
+
+}
