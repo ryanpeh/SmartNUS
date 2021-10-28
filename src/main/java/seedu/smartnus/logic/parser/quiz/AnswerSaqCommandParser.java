0e2158c7c8af1b2400f9fa25cd5f8703ccd6c371
Neo Jing Xuan <neojxuan@gmail.com>
2021-10-28 17:51:42 +0800

    feat quiz: saq answer input must start with ans/

--- src/main/java/seedu/smartnus/logic/parser/quiz/AnswerSaqCommandParser.java
+++ src/main/java/seedu/smartnus/logic/parser/quiz/AnswerSaqCommandParser.java
@@ -1,6 +1,8 @@
 package seedu.smartnus.logic.parser.quiz;
 
+import static java.util.Objects.requireNonNull;
 import static seedu.smartnus.commons.core.Messages.MESSAGE_INVALID_SAQ_ANSWER_FORMAT;
+import static seedu.smartnus.logic.parser.CliSyntax.PREFIX_ANSWER;
 
 import seedu.smartnus.logic.commands.quiz.AnswerSaqCommand;
 import seedu.smartnus.logic.parser.exceptions.ParseException;
@@ -13,10 +15,13 @@ public class AnswerSaqCommandParser implements QuizParser<AnswerSaqCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public AnswerSaqCommand parse(String args, QuizManager quizManager) throws ParseException {
-        if (!args.isBlank()) {
-            return new AnswerSaqCommand(args, quizManager);
-        } else {
+        requireNonNull(args);
+        String answerNoWhiteSpace = args.trim();
+        if (!answerNoWhiteSpace.startsWith(PREFIX_ANSWER.getPrefix())) {
             throw new ParseException(MESSAGE_INVALID_SAQ_ANSWER_FORMAT);
         }
+        // saq answers allow more flexibility by allowing ans/ to appear in the actual answer after the first prefix
+        String parsedAnswer = answerNoWhiteSpace.substring(4).trim(); //remove first occurrence of "ans/"
+        return new AnswerSaqCommand(parsedAnswer, quizManager);
     }
 }
