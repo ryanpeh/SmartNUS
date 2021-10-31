b2ab637df469ff8c72dbe85d646dfcd8b8c007ee
Ryan Peh <qwertz0808@gmail.com>
2021-11-01 02:46:16 +0800

    Fix continued validation for answered questions

--- src/main/java/seedu/smartnus/logic/parser/quiz/AnswerSaqCommandParser.java
+++ src/main/java/seedu/smartnus/logic/parser/quiz/AnswerSaqCommandParser.java
@@ -1,7 +1,10 @@
 package seedu.smartnus.logic.parser.quiz;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.smartnus.commons.core.Messages.MESSAGE_CONTINUE_QUIZ;
+import static seedu.smartnus.commons.core.Messages.MESSAGE_END_OF_QUIZ;
 import static seedu.smartnus.commons.core.Messages.MESSAGE_INVALID_SAQ_ANSWER_FORMAT;
+import static seedu.smartnus.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 import static seedu.smartnus.logic.parser.CliSyntax.PREFIX_ANSWER;
 
 import seedu.smartnus.logic.commands.quiz.AnswerSaqCommand;
@@ -17,11 +20,15 @@ public class AnswerSaqCommandParser implements QuizParser<AnswerSaqCommand> {
     public AnswerSaqCommand parse(String args, QuizManager quizManager) throws ParseException {
         requireNonNull(args);
         String answerNoWhiteSpace = args.trim();
-        if (!answerNoWhiteSpace.startsWith(PREFIX_ANSWER.getPrefix())) {
+        if (answerNoWhiteSpace.startsWith(PREFIX_ANSWER.getPrefix())) {
+            // saq answers allow more flexibility by allowing ans/ to appear in the actual answer after the first prefix
+            String parsedAnswer = answerNoWhiteSpace.substring(4).trim(); //remove first occurrence of "ans/"
+            return new AnswerSaqCommand(parsedAnswer, quizManager);
+        } else if (quizManager.isCurrentQuestionAnswered()) {
+            String endMessage = quizManager.isLastQuestion() ? MESSAGE_END_OF_QUIZ : MESSAGE_CONTINUE_QUIZ;
+            throw new ParseException(MESSAGE_UNKNOWN_COMMAND + "\n" + endMessage);
+        } else {
             throw new ParseException(MESSAGE_INVALID_SAQ_ANSWER_FORMAT);
         }
-        // saq answers allow more flexibility by allowing ans/ to appear in the actual answer after the first prefix
-        String parsedAnswer = answerNoWhiteSpace.substring(4).trim(); //remove first occurrence of "ans/"
-        return new AnswerSaqCommand(parsedAnswer, quizManager);
     }
 }
