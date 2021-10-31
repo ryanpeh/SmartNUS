376d0328eac258b85fb03df73a78bd6984f6462a
Ryan Peh <qwertz0808@gmail.com>
2021-11-01 02:39:23 +0800

    Fix continued validation for answered questions

--- src/test/java/seedu/smartnus/logic/parser/quiz/AnswerMcqCommandParserTest.java
+++ src/test/java/seedu/smartnus/logic/parser/quiz/AnswerMcqCommandParserTest.java
@@ -1,14 +1,19 @@
 package seedu.smartnus.logic.parser.quiz;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.smartnus.commons.core.Messages.MESSAGE_CONTINUE_QUIZ;
+import static seedu.smartnus.commons.core.Messages.MESSAGE_INVALID_MCQ_ANSWER_FORMAT;
+import static seedu.smartnus.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 import static seedu.smartnus.testutil.Assert.assertThrows;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import seedu.smartnus.logic.commands.quiz.AnswerMcqCommand;
 import seedu.smartnus.logic.parser.exceptions.ParseException;
 import seedu.smartnus.model.ReadOnlySmartNus;
+import seedu.smartnus.model.choice.Choice;
 import seedu.smartnus.model.quiz.QuizManager;
 import seedu.smartnus.model.util.SampleDataUtil;
 
@@ -39,10 +44,19 @@ class AnswerMcqCommandParserTest {
     }
 
     @Test
-    public void parse_invalidArgs() {
-        assertThrows(ParseException.class, () -> answerMcqCommandParser.parse("E", quizManager));
-        assertThrows(ParseException.class, () -> answerMcqCommandParser.parse("e", quizManager));
-        assertThrows(ParseException.class, () -> answerMcqCommandParser.parse("ab", quizManager));
+    public void parse_invalidArgs_notAlreadyAnswered() {
+        Exception exception = Assertions.assertThrows(ParseException.class, () ->
+                answerMcqCommandParser.parse("fasd", quizManager));
+        assertEquals(exception.getMessage(), MESSAGE_INVALID_MCQ_ANSWER_FORMAT);
+    }
+
+    @Test
+    public void parse_invalidArgs_alreadyAnswered() {
+        Choice choice = quizManager.currQuestion().getCorrectChoice();
+        quizManager.attemptAndCheckAnswer(choice);
+        Exception exception = Assertions.assertThrows(ParseException.class, () ->
+                answerMcqCommandParser.parse("fasd", quizManager));
+        assertEquals(exception.getMessage(), MESSAGE_UNKNOWN_COMMAND + "\n" + MESSAGE_CONTINUE_QUIZ);
     }
 
 
