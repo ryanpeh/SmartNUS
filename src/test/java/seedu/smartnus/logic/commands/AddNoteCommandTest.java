7ff29e0f49279a4c98a26cca3c1fdeaaa8f55f39
Agentum07 <sheamus9712@gmail.com>
2021-11-05 23:04:33 +0800

    update model Stub

--- src/test/java/seedu/smartnus/logic/commands/AddNoteCommandTest.java
+++ src/test/java/seedu/smartnus/logic/commands/AddNoteCommandTest.java
@@ -2,6 +2,8 @@ package seedu.smartnus.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.smartnus.testutil.Assert.assertThrows;
 
 import java.nio.file.Path;
@@ -15,6 +17,7 @@ import org.junit.jupiter.api.Test;
 import javafx.collections.ObservableList;
 import seedu.smartnus.commons.core.GuiSettings;
 import seedu.smartnus.commons.core.theme.Theme;
+import seedu.smartnus.logic.commands.questions.AddMcqCommand;
 import seedu.smartnus.model.Model;
 import seedu.smartnus.model.ReadOnlySmartNus;
 import seedu.smartnus.model.ReadOnlyUserPrefs;
@@ -23,6 +26,7 @@ import seedu.smartnus.model.note.Note;
 import seedu.smartnus.model.question.Question;
 import seedu.smartnus.model.statistic.TagStatistic;
 import seedu.smartnus.testutil.NoteBuilder;
+import seedu.smartnus.testutil.QuestionBuilder;
 
 public class AddNoteCommandTest {
     @Test
@@ -32,7 +36,7 @@ public class AddNoteCommandTest {
 
     @Test
     public void execute_noteAcceptedByModel_addSuccessful() throws Exception {
-        ModelStubAcceptingNoteAdded modelStub = new ModelStubAcceptingNoteAdded();
+        AddNoteCommandTest.ModelStubAcceptingNoteAdded modelStub = new AddNoteCommandTest.ModelStubAcceptingNoteAdded();
         Note validNote = new NoteBuilder().build();
 
         CommandResult commandResult = new AddNoteCommand(validNote).execute(modelStub);
@@ -41,6 +45,30 @@ public class AddNoteCommandTest {
         assertEquals(Arrays.asList(validNote), modelStub.notesAdded);
     }
     // todo: if duplicate note checker is implemented, add a test here to check if it works properly
+
+    @Test
+    public void equals() {
+        Note alice = new NoteBuilder().withName("Alice").build();
+        Note bob = new NoteBuilder().withName("Bob").build();
+        AddNoteCommand addAliceCommand = new AddNoteCommand(alice);
+        AddNoteCommand addBobCommand = new AddNoteCommand(bob);
+
+        // same object -> returns true
+        assertTrue(addAliceCommand.equals(addAliceCommand));
+
+        // same values -> returns true
+        AddNoteCommand addAliceCommandCopy = new AddNoteCommand(alice);
+        assertTrue(addAliceCommand.equals(addAliceCommandCopy));
+
+        // different types -> returns false
+        assertFalse(addAliceCommand.equals(1));
+
+        // null -> returns false
+        assertFalse(addAliceCommand.equals(null));
+
+        // different question -> returns false
+        assertFalse(addAliceCommand.equals(addBobCommand));
+    }
 
     private class ModelStub implements Model {
         @Override
@@ -195,7 +223,7 @@ public class AddNoteCommandTest {
     }
 
     /**
-     * A Model stub that always accept the question being added.
+     * A Model stub that always accept the note being added.
      */
     private class ModelStubAcceptingNoteAdded extends ModelStub {
         final ArrayList<Note> notesAdded = new ArrayList<>();
